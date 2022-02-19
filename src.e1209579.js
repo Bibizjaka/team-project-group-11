075e7f49f9c98af6329410b80bf704499e966a06
serj-nobody <81528207+serj-nobody@users.noreply.github.com>
2022-02-19 15:37:42 +0000

    Deploying to gh-pages from @ Bibizjaka/team-project-group-11@4bd055cd8f1b40e55477cd9729b1374dcffe1133 🚀

--- src.e1209579.js
+++ src.e1209579.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/hero/arrow-right-tb.png":[["arrow-right-tb.f441fb55.png","vKPg"],"vKPg"],"./../images/hero/arrow-right-tb@2x.png":[["arrow-right-tb@2x.51e98e8c.png","mRSo"],"mRSo"],"./../images/hero/arrow-right-dt.png":[["arrow-right-dt.e755e3a9.png","lMAX"],"lMAX"],"./../images/hero/arrow-right-dt@2x.png":[["arrow-right-dt@2x.add5b01a.png","UXYa"],"UXYa"],"./../images/advantages/cow-bucket.png":[["cow-bucket.28b56f7e.png","KGsk"],"KGsk"],"./../images/advantages/apple-bucket.png":[["apple-bucket.7b20a05f.png","QtFd"],"QtFd"],"./../images/advantages/ice-bucket.png":[["ice-bucket.c76fbf7b.png","siWn"],"siWn"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-group-11/src.7a890ec0.js.map
+//# sourceMappingURL=/team-project-group-11/src.e1209579.js.map
